7e45221da6ced3a2d020f2c9ddc566a482813770
Pavan K Jadda <pavankjadda@users.noreply.github.com>
2020-08-09 02:23:06 -0400

    Adds icon for side nav

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,4 +1,4 @@
-import {createStyles, IconButton, Theme} from '@material-ui/core';
+import {createStyles, Theme} from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button';
 import makeStyles from '@material-ui/core/styles/makeStyles';
@@ -29,9 +29,7 @@ export default function Header()
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <SideMenu/>
-                    </IconButton>
+                    <SideMenu/>
                     <Typography variant="h6" className={classes.title}>
                         React Crud App
                     </Typography>
--- src/layout/side-menu/side-menu.tsx
+++ src/layout/side-menu/side-menu.tsx
@@ -1,16 +1,18 @@
-import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import {makeStyles} from '@material-ui/core/styles';
 import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import clsx from 'clsx';
 import React from 'react';
 import "./side-menu.scss"
+
 
 const useStyles = makeStyles({
     list: {
@@ -18,6 +20,9 @@ const useStyles = makeStyles({
     },
     fullList: {
         width: 'auto',
+    },
+    menuIcon: {
+        color: 'white'
     },
 });
 
@@ -33,31 +38,22 @@ export default function SideMenu()
         right: false,
     });
 
-    const toggleDrawer = (anchor: Anchor, open: boolean) => (
-        event: React.KeyboardEvent | React.MouseEvent,
-    ) =>
+    const toggleDrawer = (anchor: Anchor, open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) =>
     {
-        if (
-            event.type === 'keydown' &&
-            ((event as React.KeyboardEvent).key === 'Tab' ||
-                (event as React.KeyboardEvent).key === 'Shift')
-        )
+        if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift'))
         {
             return;
         }
-
         setState({...state, [anchor]: open});
     };
 
     const list = (anchor: Anchor) => (
-        <div
-            className={clsx(classes.list, {
-                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-            })}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
+        <div className={clsx(classes.list, {
+            [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+        })}
+             role="presentation"
+             onClick={toggleDrawer(anchor, false)}
+             onKeyDown={toggleDrawer(anchor, false)}>
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem button key={text}>
@@ -82,9 +78,9 @@ export default function SideMenu()
         <div>
             {(['left'] as Anchor[]).map((anchor) => (
                 <React.Fragment key={anchor}>
-                    <Button onClick={toggleDrawer(anchor, true)}>
-                        {anchor}
-                    </Button>
+                    <IconButton aria-label="delete" onClick={toggleDrawer(anchor, true)}>
+                        <MenuIcon className={classes.menuIcon}/>
+                    </IconButton>
                     <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
                         {list(anchor)}
                     </Drawer>
