71a729de868d9f6c98123cfb7da3d5271f90c050
Pavan K Jadda <pavankjadda@users.noreply.github.com>
2020-08-09 03:47:44 -0400

    Fixes routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import {Grid} from '@material-ui/core';
 import React from 'react';
 import './App.scss';
 import Employees from './employee/employees';
-import Footer from './layout/footer/footer';
-import Header from './layout/header/header';
 
 class App extends React.Component
 {
@@ -11,11 +9,9 @@ class App extends React.Component
     {
         return (
             <div className="App">
-                <Header/>
                 <Grid id="content" className="content">
                     <Employees/>
                 </Grid>
-                <Footer/>
             </div>
         );
     }
--- src/AppRouting.tsx
+++ src/AppRouting.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import App from './App';
 import EmployeeCreate from './employee/employee-create';
 import Employees from './employee/employees';
+import Footer from './layout/footer/footer';
+import Header from './layout/header/header';
 
 
 export default class AppRouting extends React.Component
@@ -12,17 +14,20 @@ export default class AppRouting extends React.Component
         return (
             <Router>
                 <div>
+                    <Header/>
                     <Switch>
                         <Route exact path="/">
                             <App/>
                         </Route>
-                        <Route path="/employee">
-                            <Employees/>
-                        </Route>
                         <Route path="/employee/create">
                             <EmployeeCreate/>
                         </Route>
+                        <Route path="/employee">
+                            <Employees/>
+                        </Route>
+
                     </Switch>
+                    <Footer/>
                 </div>
             </Router>
         );
